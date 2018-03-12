da730c9f29a21f9958ae0b6ac196b9a3ee1e35d6
LucienBouletRoblin <lucien.boulet-roblin@capgemini.com>
2018-03-13 00:25:39 +0100

    adding redux offline

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,15 +1,17 @@
-import { applyMiddleware, createStore } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import thunk from 'redux-thunk';
+import { applyMiddleware, createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import offlineConfig from "@redux-offline/redux-offline/lib/defaults";
+import { offline } from "@redux-offline/redux-offline";
+import thunk from "redux-thunk";
 
-import { loadState } from '../localStorage';
+import { loadState } from "../localStorage";
 
-import reducers from '../reducers';
+import reducers from "../reducers";
 
 const persistedState = loadState();
 
 export default createStore(
   reducers,
   persistedState,
-  composeWithDevTools(applyMiddleware(thunk))
+  composeWithDevTools(applyMiddleware(thunk), offline(offlineConfig))
 );
